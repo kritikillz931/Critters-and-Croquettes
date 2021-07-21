f8c3692f0932fe57b56c7daecd1000a3d71d7279
Justin <justinadams758@gmail.com>
2021-07-21 15:18:10 -0500

    critters have been fed

--- Models/glassTank.py
+++ Models/glassTank.py
@@ -2,18 +2,21 @@ from datetime import date
 
 class GlassTankAreaAnimal:
 
-    def __init__(self, id, name, species, shift):
+    def __init__(self, id, name, species, shift, food):
         self.id = id
         self.name = name
         self.species = species
         self.slithering = True
         self.dateAdded = date.today()
         self.shift = shift
+        self.food = food
 
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
-Patrick = GlassTankAreaAnimal("1", "Patrick", "Python", "Evening")
-Kobe = GlassTankAreaAnimal("2", "Kobe", "Mamba", "Evening")
-Satan = GlassTankAreaAnimal("3", "Satan", "Garden Snake", "Evening")
-Dakota = GlassTankAreaAnimal("4", "Dakota", "Anaconda", "Evening")
-Richard = GlassTankAreaAnimal("5", "Richard", "Viper", "Evening")
+Patrick = GlassTankAreaAnimal("1", "Patrick", "Python", "Evening", "Mice")
+Kobe = GlassTankAreaAnimal("2", "Kobe", "Mamba", "Evening", "Mice")
+Satan = GlassTankAreaAnimal("3", "Satan", "Garden Snake", "Evening", "Mice")
+Dakota = GlassTankAreaAnimal("4", "Dakota", "Anaconda", "Evening", "Mice")
+Richard = GlassTankAreaAnimal("5", "Richard", "Viper", "Evening", "Mice")
 
