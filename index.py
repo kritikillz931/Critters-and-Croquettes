fb5d3faf07496df37cbe699cf60d8663b569e3ac
Justin <justinadams758@gmail.com>
2021-07-23 10:15:47 -0500

    saving changes

---
+++ index.py
@@ -0,0 +1,2 @@
+from animals import Animal, AnglerFish, Betafish, Camel, Cat, Clownfish, Elephant, Giraffe, Goldfish, Horse, Monkey, Shark, Snake
+from attractions import SnakePit, PettingZoo, Wetlands, Attraction
